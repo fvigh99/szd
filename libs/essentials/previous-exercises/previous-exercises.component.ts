8d4472d9608a7301d4bc97e6a6a7b80830e97b23
fvigh99 <ferencvigh99@gmail.com>
2024-04-29 04:45:30 +0200

    Implemented different templates based on user roles

--- libs/essentials/previous-exercises/previous-exercises.component.ts
+++ libs/essentials/previous-exercises/previous-exercises.component.ts
@@ -3,7 +3,7 @@ import { AsyncPipe, CommonModule, NgIf } from '@angular/common';
 import { TableModule } from 'primeng/table';
 import { ExerciseService } from 'libs/data-access/exercise/exercise.service';
 import { Observable } from 'rxjs';
-import { Exercise, Machine } from 'libs/model/FcServerModel';
+import { Exercise, Machine, User } from 'libs/model/FcServerModel';
 import { ToolbarModule } from 'primeng/toolbar';
 import { ButtonModule } from 'primeng/button';
 import { MessageService } from 'primeng/api';
@@ -18,6 +18,8 @@ import {
   AutoCompleteModule,
 } from 'primeng/autocomplete';
 import { MachineService } from 'libs/data-access/machine/machine.service';
+import { AccountService } from 'libs/data-access/account/account.service';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @Component({
   selector: 'fc-previous-exercises',
@@ -36,6 +38,7 @@ import { MachineService } from 'libs/data-access/machine/machine.service';
     FormsModule,
     CalendarModule,
     AutoCompleteModule,
+    InputNumberModule,
   ],
   templateUrl: './previous-exercises.component.html',
   styleUrl: './previous-exercises.component.scss',
@@ -47,20 +50,26 @@ export class PreviousExercisesComponent implements OnInit {
   public machineList: any;
   public selectedMachine: Machine;
   public filteredMachines: any[] | undefined;
+  public loggedInUser: User;
   constructor(
     private exerciseService: ExerciseService,
     private messageService: MessageService,
-    private machineService: MachineService
+    private machineService: MachineService,
+    private accountService: AccountService
   ) {}
   ngOnInit(): void {
     this.fetchData();
     this.machineService.fetch().subscribe((result) => {
       this.machineList = result;
     });
+    this.loggedInUser = this.accountService.userValue.user_object;
   }
 
   public fetchData() {
     this.exerciseList = this.exerciseService.fetch();
+    this.exerciseList.subscribe((result) => {
+      console.log(result);
+    });
   }
 
   public filterMachine(event: AutoCompleteCompleteEvent) {
@@ -89,6 +98,11 @@ export class PreviousExercisesComponent implements OnInit {
       (this.newExercise.weight && this.newExercise.count) ||
       (this.newExercise.intensity && this.newExercise.duration)
     ) {
+      this.newExercise.user = this.loggedInUser;
+      this.newExercise.machine = this.selectedMachine.id
+        ? this.selectedMachine
+        : null;
+      console.log(this.newExercise);
       this.exerciseService.create(this.newExercise).subscribe((result) => {
         if (result) {
           this.messageService.add({
@@ -113,7 +127,8 @@ export class PreviousExercisesComponent implements OnInit {
       this.messageService.add({
         severity: 'error',
         detail: 'Hiba!',
-        summary: 'Hibás adatok!',
+        summary:
+          'Hibás adatok! A következő adatokpárokból egyet, és csak egyet kell megadnia: súly-ismétlés / intenzitás-időtartam!',
       });
     }
   }
